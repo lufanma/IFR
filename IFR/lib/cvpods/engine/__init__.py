9098a1260d43a6139274221080cb897ff6e93fdc
lufanma <malufanjune@163.com>
2021-10-27 15:54:51 +0800

    del redundant IFR directory

--- IFR/lib/cvpods/engine/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-
-from .base_runner import *
-from .runner import *
-
-__all__ = [k for k in globals().keys() if not k.startswith("_")]
--- IFR/mask_rcnn.ifr.res50.fpn.coco.multiscale.1x/pods_train.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export NCCL_IB_DISABLE=1
-PYTHONPATH=./:$PYTHONPATH OMP_NUM_THREADS=1 python3 ../tools/train_net.py --num-gpus 8
